901712bb0e4d2cd0e3ff312c285b1896d2f79e77
Sota ogasawara <86047097+Mannnenn@users.noreply.github.com>
2024-09-04 19:22:52 +0900

    Add control

--- src/main_control.cpp
+++ src/main_control.cpp
@@ -10,11 +10,27 @@ class MyNode : public rclcpp::Node
 public:
     MyNode() : Node("my_node")
     {
+        // パラメータの宣言
+        this->declare_parameter<std::string>("input_propo_topic_name", "/command");
+        this->declare_parameter<std::string>("output_command_explicit_topic_name", "/command_explicit");
+        this->declare_parameter<std::string>("output_mode_topic_name", "/mode");
+
+        // パラメータの取得
+        std::string input_propo_topic_name;
+        this->get_parameter("input_propo_topic_name", input_propo_topic_name);
+        std::string output_command_explicit_topic_name;
+        this->get_parameter("output_command_explicit_topic_name", output_command_explicit_topic_name);
+        std::string output_mode_topic_name;
+        this->get_parameter("output_mode_topic_name", output_mode_topic_name);
+
         string_subscriber_ = this->create_subscription<std_msgs::msg::String>(
             "string_topic", 10, std::bind(&MyNode::string_callback, this, std::placeholders::_1));
 
         rpy_subscriber_ = this->create_subscription<nokolat2024_msg::msg::Rpy>(
             "rpy_topic", 10, std::bind(&MyNode::rpy_callback, this, std::placeholders::_1));
+
+        altitude_subscriber_ = this->create_subscription<geometry_msgs::msg::PointStamped>(
+            "altitude_stamped_topic", 10, std::bind(&MyNode::altitude_callback, this, std::placeholders::_1));
 
         command_publisher_ = this->create_publisher<nokolat2024_msg::msg::Command>("command_topic", 10);
     }
