930956a097eefc2f7d253def70520a85f494a7c0
Sota ogasawara <86047097+Mannnenn@users.noreply.github.com>
2024-09-11 20:40:56 +0900

    Fix param bag

--- include/nokolat2024/main_control.hpp
+++ include/nokolat2024/main_control.hpp
@@ -6,7 +6,6 @@
 #include <deque>
 
 #include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/float32.hpp"
 #include "geometry_msgs/msg/point_stamped.hpp"
 
 #include "nokolat2024_msg/msg/rpy.hpp"
@@ -29,44 +28,31 @@ namespace nokolat2024
             AUTO_RISE_TURNING = 2,
             AUTO_LANDING = 3,
             AUTO_EIGHT = 4,
-            AUTO_STRAIGHT = 5,
-            AUTO_TURNING_R = 6,
-        };
-
-        enum EIGHT_TURN_MODE
-        {
-            LEFT_TURN = -1,
-            NEUTRAL = 0,
-            RIGHT_TURN = 1,
         };
 
         // マップを初期化
-
-        const std::unordered_map<int16_t, std::string>
-            control_mode_map = {
-                {CONTROL_MODE::MANUAL, "MANUAL"},
-                {CONTROL_MODE::AUTO_TURNING, "AUTO_TURNING"},
-                {CONTROL_MODE::AUTO_RISE_TURNING, "AUTO_RISE_TURNING"},
-                {CONTROL_MODE::AUTO_LANDING, "AUTO_LANDING"},
-                {CONTROL_MODE::AUTO_EIGHT, "AUTO_EIGHT"},
-                {CONTROL_MODE::AUTO_STRAIGHT, "AUTO_STRAIGHT"},
-                {CONTROL_MODE::AUTO_TURNING_R, "AUTO_TURNING_R"}};
+        const std::unordered_map<int16_t, std::string> control_mode_map = {
+            {CONTROL_MODE::MANUAL, "MANUAL"},
+            {CONTROL_MODE::AUTO_TURNING, "AUTO_TURNING"},
+            {CONTROL_MODE::AUTO_RISE_TURNING, "AUTO_RISE_TURNING"},
+            {CONTROL_MODE::AUTO_LANDING, "AUTO_LANDING"},
+            {CONTROL_MODE::AUTO_EIGHT, "AUTO_EIGHT"}};
 
         struct ControlInfo_config
         {
-            int throttle_max;
-            int throttle_min;
-            int elevator_max;
-            int elevator_min;
-            int aileron_max_r;
-            int aileron_min_r;
-            int aileron_max_l;
-            int aileron_min_l;
-            int rudder_max;
-            int rudder_min;
-            int drop_max;
-            int drop_center;
-            int drop_min;
+            double throttle_max;
+            double throttle_min;
+            double elevator_max;
+            double elevator_min;
+            double aileron_max_r;
+            double aileron_min_r;
+            double aileron_max_l;
+            double aileron_min_l;
+            double rudder_max;
+            double rudder_min;
+            double drop_max;
+            double drop_center;
+            double drop_min;
         };
 
         struct ControlInfo_gain
@@ -77,28 +63,22 @@ namespace nokolat2024
             double pitch_gain;
         };
 
-        struct ControlInfo_gain_lr : public ControlInfo_gain
-        {
-            ControlInfo_gain l;
-            ControlInfo_gain r;
-        };
-
         struct ControlInfo_target
         {
             double velocity_target;
             double altitude_target;
             double roll_target;
             double pitch_target;
-            double yaw_target;
             double throttle_target;
             double rudder_target;
         };
 
         struct ControlInfo_target_lr : public ControlInfo_target
         {
-            ControlInfo_target l;
-            ControlInfo_target r;
-            ControlInfo_target common;
+            double roll_target_l;
+            double roll_target_r;
+            double rudder_target_l;
+            double rudder_target_r;
         };
         struct Command
         {
@@ -120,13 +100,8 @@ namespace nokolat2024
 
         struct DelayWindow
         {
-            double delay_rudder;
-        };
-
-        struct DelayWindow_lr : public DelayWindow
-        {
-            DelayWindow l;
-            DelayWindow r;
+            uint delay_rudder;
+            uint delay_rudder_counter;
         };
     } // namespace main_control
 } // namespace nokolat2024
